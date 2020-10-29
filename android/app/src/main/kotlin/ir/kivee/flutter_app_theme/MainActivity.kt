81139da17436fe454f658943a2a914361340e6f3
p4yaam <p4yaam@gmail.com>
2020-10-29 22:22:41 +0330

    initial commit

---
+++ android/app/src/main/kotlin/ir/kivee/flutter_app_theme/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.kivee.flutter_app_theme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc/theme_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_theme/bloc/theme_event.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+class ThemeBloc extends HydratedBloc<ThemeEvent, ThemeData> {
+  ThemeBloc() : super(ThemeData.light());
+
+  @override
+  Stream<ThemeData> mapEventToState(ThemeEvent event) async* {
+    if (event is LightThemeEvent) yield ThemeData.light();
+    if (event is DarkThemeEvent) yield ThemeData.dark();
+  }
+
+  @override
+  ThemeData fromJson(Map<String, dynamic> json) {
+    try {
+      if (json['light'] as bool) return ThemeData.light();
+      return ThemeData.dark();
+    } catch (_) {
+      return null;
+    }
+  }
+
+  @override
+  Map<String, bool> toJson(ThemeData state) {
+    try {
+      return {'light': state == ThemeData.light()};
+    } catch (_) {
+      return null;
+    }
+  }
+}
